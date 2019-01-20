f0c2e8b0c09b9528cf71b22e1a6cab832733bfc0
Tyler Suehr <tylersuehr@gmail.com>
2019-01-20 00:30:04 -0500

    Updated some code and docs

--- src/main/com/tylersuehr/sql/SQLiteOpenHelper.java
+++ src/main/com/tylersuehr/sql/SQLiteOpenHelper.java
@@ -3,12 +3,9 @@ import java.io.Closeable;
 import java.io.File;
 
 /**
- * Copyright 2017 Tyler Suehr
- * Created by tyler on 8/23/2017.
+ * Manages the SQLite database file, allowing it to be versioned.
  *
- * This manages the SQLite database file and allows it to be versioned.
- *
- * Using this helper, we can utilize SQLite for the following:
+ * This helper affords the following database techniques:
  * (1) Code-First: Create a database during runtime.
  * (2) Database-First: Use an existing SQLite database file during runtime.
  *
@@ -35,20 +32,25 @@ import java.io.File;
  * {@link #onUpdate(SQLiteDatabase, int, int)}, if your given version was higher than the
  * user_version. This can be used to drop all the tables and re-create them if you've updated
  * the table structure.
+ *
+ * @author Tyler Suehr
  */
 public abstract class SQLiteOpenHelper implements Closeable {
+    /* Stores reference to the SQLite database instance */
     private SQLiteDatabase database;
+    /* Stores version of the SQLite database */
     private int version;
+    /* Stores name of the SQLite database */
     private String name;
 
 
-    public SQLiteOpenHelper(String dbName, int version) {
+    public SQLiteOpenHelper(final String dbName, final int version) {
         this.name = (dbName.contains(".db") ? dbName : dbName.concat(".db"));
         this.version = version;
     }
 
     @Override
-    public void close() {
+    public final void close() {
         if (database != null) {
             this.database.close();
         }
@@ -56,26 +58,27 @@ public abstract class SQLiteOpenHelper implements Closeable {
 
     /**
      * Called when a new database is created.
-     * @param db Database created
+     * @param db the created database
      */
     protected abstract void onCreate(SQLiteDatabase db);
 
     /**
      * Called when a database is updated.
-     * @param db Updated database
-     * @param oldV Old database user version
-     * @param newV New database user version
+     *
+     * @param db the updated database
+     * @param oldV the old user version number
+     * @param newV the new user version number
      */
     protected abstract void onUpdate(SQLiteDatabase db, int oldV, int newV);
 
     /**
-     * Lazy-loads our SQLite database and ensures that only one instance is available.
-     * @return {@link SQLiteDatabase}
+     * Lazily loads the SQLite database, ensuring only one instance is available.
+     * @return the SQLite database
      */
-    public SQLiteDatabase getWritableInstance() {
+    public final SQLiteDatabase getWritableInstance() {
         if (database == null) {
-            File file = new File(name);
-            boolean alreadyExists = file.exists();
+            final File file = new File(name);
+            final boolean alreadyExists = file.exists();
 
             // This creates our SQLite database file for us, so check if it
             // already exists before this call.
@@ -84,17 +87,17 @@ public abstract class SQLiteOpenHelper implements Closeable {
             // Check if the database file already exists
             if (alreadyExists) {
                 // Check if the database should be updated
-                int curVersion = database.getVersion();
+                final int curVersion = database.getVersion();
                 if (version > curVersion) {
                     onUpdate(database, curVersion, version);
                     this.database.setVersion(version);
-                    Log.i(this, "Database updated!");
+                    System.out.println("SQLite database updated!");
                 }
             } else {
                 // Create our database, since it doesn't exist
                 onCreate(database);
                 this.database.setVersion(version);
-                Log.i(this, "Database created!");
+                System.out.println("SQLite database created!");
             }
         }
         return database;

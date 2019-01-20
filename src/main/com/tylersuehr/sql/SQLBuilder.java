6515813102ac7ac2e80db5b1990607c2ed5057a5
Tyler Suehr <tylersuehr@gmail.com>
2019-01-20 00:12:56 -0500

    Updated some code and docs

--- src/main/com/tylersuehr/sql/SQLBuilder.java
+++ src/main/com/tylersuehr/sql/SQLBuilder.java
@@ -1,33 +1,24 @@
 package com.tylersuehr.sql;
 
 /**
- * Copyright 2017 Tyler Suehr
- * Created by tyler on 8/23/2017.
- *
- * Utility to help build SQL queries and commands.
+ * Utility to help construct SQL queries and commands.
+ * @author Tyler Suehr
  */
 final class SQLBuilder {
     // SELECT * FROM [table] WHERE [col1] = value ORDER BY [col] LIMIT 0;
     String createQuery(String table, String selection, String order, String limit) {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         sb.append("SELECT * FROM ").append("[").append(table).append("]");
-
-        String temp = (selection != null ? " WHERE " + selection : "");
-        sb.append(temp);
-
-        temp = (order != null ? " ORDER BY " + order : "");
-        sb.append(temp);
-
-        temp = (limit != null ? " LIMIT " + limit : "");
-        sb.append(temp);
-
+        sb.append(selection != null ? " WHERE " + selection : "");
+        sb.append(order != null ? " ORDER BY " + order : "");
+        sb.append(limit != null ? " LIMIT " + limit : "");
         sb.append(";");
         return sb.toString();
     }
 
     // SELECT ([col1],[col2],[col3]) FROM [table] WHERE [col] = value ORDER BY [col] LIMIT 0;
     String createQuery(String table, String[] cols, String selection, String order, String limit) {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         sb.append("SELECT ");
 
         if (cols != null) {
@@ -44,22 +35,16 @@ final class SQLBuilder {
         }
         sb.append("[").append(table).append("]");
 
-        String temp = (selection != null ? " WHERE " + selection : "");
-        sb.append(temp);
-
-        temp = (order != null ? " ORDER BY " + order : "");
-        sb.append(temp);
-
-        temp = (limit != null ? " LIMIT " + limit : "");
-        sb.append(temp);
-
+        sb.append(selection != null ? " WHERE "  + selection : "");
+        sb.append(order != null ? " ORDER BY " + order : "");
+        sb.append(limit != null ? " LIMIT " + limit : "");
         sb.append(";");
         return sb.toString();
     }
 
     // INSERT INTO [table] ([col1],[col2],[col3],[col4]) VALUES ('test', 'test2', 123, 12.123);
     String createInsert(String table, ContentValues values) {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         sb.append("INSERT INTO ");
         sb.append("[").append(table).append("] (");
 
@@ -87,7 +72,7 @@ final class SQLBuilder {
 
     // UPDATE [table] SET [col1] = 'test' WHERE [col2] = 3;
     String createUpdate(String table, ContentValues values, String selection) {
-        StringBuilder sb = new StringBuilder(120);
+        final StringBuilder sb = new StringBuilder(120);
         sb.append("UPDATE ");
         sb.append("[").append(table).append("]");
         sb.append(" SET ");
@@ -106,26 +91,17 @@ final class SQLBuilder {
             i++;
         }
 
-        if (selection != null) {
-            sb.append(" WHERE ");
-            sb.append(selection);
-        }
-
+        sb.append(selection != null ? " WHERE " + selection : "");
         sb.append(";");
         return sb.toString();
     }
 
     // DELETE FROM [table] WHERE [col1] = 23;
     String createDelete(String table, String selection) {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
         sb.append("DELETE FROM ");
         sb.append("[").append(table).append("]");
-
-        if (selection != null) {
-            sb.append(" WHERE ");
-            sb.append(selection);
-        }
-
+        sb.append(selection != null ? " WHERE " + selection : "");
         sb.append(";");
         return sb.toString();
     }
